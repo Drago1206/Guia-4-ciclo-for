c174e84829e30214586f1e4abb8bdbad17829ea8
JhoanSebastianGelvesHernándezADSO <Jhoangelves1205@gmail.com>
2022-10-04 19:29:42 -0500

    2

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 addEventListener("DOMContentLoaded",()=>{
+// ##GENERAR LA SERIE 1,5,3,7,5,,9,7...23
+document.write(`GENERAR LA SERIE 1,5,3,7,5,,9,7...23<br>`)
 for(let i=1; i<22; i+=2 ){
     let s=(i+4-2)
     document.write(`${s}<br>`)
