6ebe1a9ecb0332f870248f72d8034e771537c8ce
JhoanSebastianGelvesHernándezADSO <Jhoangelves1205@gmail.com>
2022-10-17 19:07:42 -0500

    4.1

--- main.js
+++ main.js
@@ -1,28 +1,14 @@
 addEventListener("DOMContentLoaded",()=>{
-let est=0,estu=0,enc=0,calf=0
-alert(`BIENVENIDO CALIFIQUE LA COMIDA TENIENDO ENCUENTA QUE DEL 1 AL 6 ES HORRIBLE Y DEL 7 AL 10 EXCELENTE (PORFAVOR NO TIRE TAN DURO :D)`)
-est=Number(prompt(`Ingrese la cantidad de personas que realizaran la encuesta: `))
-document.write(`La cantidad de entrevistados ${est}<br>`)
-
-for(let i=0;i<est;++i){
-    enc=Number(prompt(`Ingrese la calificacion: `));
-    if(enc>=7 && enc<= 10){
-        document.write(`Es buena<br>`)
-        let notam=0
-        enc=notam
-        estu=estu+est
-        calf=calf+enc
+let M=0,Tl=0
+for(let i=0;i<21;i++){
+    if(i==1){
+        M=10
     }
-    else if(enc>0 && enc<= 6);{
-        document.write(`Es horrible<br>`)
-        let notamn=enc
-        estu=estu+est
-        calf=calf+enc
+    else{
+        M=M*2
+        Tl=Tl+M
     }
-    
+    document.write(`El pago en el mes ${i}<br>`)
+    document.write(`El pago total es de: ${Tl}<br>`)
 }
-let prom=(calf/estu)
-document.write(`El promedio de estudiantes que realizaron la encuesta es de: ${prom}<br>`)
-document.write(`La nota mayor es de: ${notam}`)
-document.write(`La nota menor es de: ${notamn}`)
 })
